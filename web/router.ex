f67752e7ab7f7a17bdef1a4080b362a3f6bcfe80
Luiz Paulo Godoy <luizpaulogodoy@gmail.com>
2016-09-19 22:52:22 -0300

    Creating Index route

--- web/router.ex
+++ web/router.ex
@@ -16,7 +16,8 @@ defmodule PhBlog.Router do
   scope "/", PhBlog do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/", IndexController, :index
+    #get "/", PageController, :index
   end
 
   # Other scopes may use custom stacks.
